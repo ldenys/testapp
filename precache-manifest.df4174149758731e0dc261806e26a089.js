922153fca618885ae1c1c37051290d25a6b00ef3
unknown <waldensony@gmail.com>
2019-11-30 17:42:49 +0200

    Updates

---
+++ precache-manifest.df4174149758731e0dc261806e26a089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d424403a7764dcb4bfabe30a9f06ac",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "a1a79fd2c8eec72f1815",
+    "url": "/testapp/static/css/main.2a14f9bc.chunk.css"
+  },
+  {
+    "revision": "c342842c054708a73f0d",
+    "url": "/testapp/static/js/2.d176d321.chunk.js"
+  },
+  {
+    "revision": "a1a79fd2c8eec72f1815",
+    "url": "/testapp/static/js/main.36f7dc3d.chunk.js"
+  },
+  {
+    "revision": "112efacf959461a75352",
+    "url": "/testapp/static/js/runtime-main.0c0f5a6e.js"
+  },
+  {
+    "revision": "261bc4dd90a5b229b682547bec804fca",
+    "url": "/testapp/static/media/preloader.261bc4dd.svg"
+  }
+]);
